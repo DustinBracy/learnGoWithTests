8135bbfe3fc7e8b25462ba43d41540c2318d506f
Dustin Bracy <dustin@bracytechnology.com>
2026-02-04 18:11:46 +0000

    complete structs

---
+++ structs/shapes.go
@@ -0,0 +1,39 @@
+package structs
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.141592653589793 * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Length + r.Width)
+}
+
+func Area(r Rectangle) float64 {
+	return r.Length * r.Width
+}
