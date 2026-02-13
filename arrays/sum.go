360bcabb121ed92d6516428e2c1d47d09ceb8052
DustinBracy <dustin@bracytechnology.com>
2026-02-12 20:59:01 -0600

    pre-refactor of generic revision

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,16 +1,9 @@
 package arrays
 
-// Sum returns the sum of a slice of integers.
+// Sum calculates the total from a slice of numbers.
 func Sum(numbers []int) int {
-	add := func(acc, x int) int {
-		return acc + x
-	}
-	return Reduce(numbers, add)
-	// sum := 0
-	// for _, number := range numbers {
-	// 	sum += number
-	// }
-	// return sum
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
 }
 
 func SumAll(numbersToSum ...[]int) []int {
@@ -22,35 +15,43 @@ func SumAll(numbersToSum ...[]int) []int {
 	return sums
 }
 
-func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+// SumAllTails calculates the sums of all but the first number given a collection of slices.
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
 
-	return sums
+	return Reduce(numbers, sumTail, []int{})
 }
 
-func Reduce[T any](collection []T, reducer func(T, T) T) T {
-	if len(collection) == 0 {
-		var zeroValue T
-		return zeroValue
+func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
 	}
-	var value T
+	return result
+}
 
-	for i, item := range collection {
-		if i == 0 {
-			value = item
-			continue
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
 		}
-
-		value = reducer(value, collection[i])
+		if t.To == name {
+			balance += t.Sum
+		}
 	}
-	return value
-
+	return balance
 }
